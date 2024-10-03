5b0a1e863ae374be26bcedcb989c76b04efe3de9
wadi kihel <wadi.kihel@epitech.eu>
2024-10-03 23:35:41 +0000

    favicon

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,32 @@
-FROM node:17.0.1-alpine
-RUN apk update && apk add bash
+# Étape 1 : Builder l'application Nuxt.js
+FROM node:18-alpine AS builder
+
 WORKDIR /usr/src/app
-COPY ./package.json /usr/src/app/package.json
+
+# Installer les dépendances
+COPY package*.json ./
 RUN npm install
-EXPOSE 9444
+
+# Mettre à jour la base de données de browserslist
+RUN npx browserslist@latest --update-db
+
+# Copier le reste des fichiers et construire l'application
+COPY . .
+RUN npm run build
+
+# Étape 2 : Image de production
+FROM node:18-alpine
+
+WORKDIR /usr/src/app
+
+# Copier les fichiers construits depuis l'étape de build
+COPY --from=builder /usr/src/app/.output ./
+
+# Installer uniquement les dépendances de production
+RUN npm install --production
+
+# Exposer le port
+EXPOSE 3002
+
+# Démarrer l'application
+CMD ["node", "server/index.mjs"]
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,40 +2,40 @@ import path from 'path';
 import fs from 'fs';
 
 process.env.PORT = "3002";
+
 export default {
-    //target: 'static',
-    //router: { base: '' },
     build: {
-      standalone: true 
+        standalone: true 
     },
     bridge: false,
-    head : {
-      script : {
-        type: "text/javascript"
-      },
-      link: [
-        { rel: 'icon', type: 'image/png', href: '~/assets/favicon.png' },
-      ]
+    head: {
+        script: {
+            type: "text/javascript"
+        },
+        link: [
+            { rel: 'icon', type: 'image/png', href: '~/assets/favicon.png' },
+        ]
     },
     css: ['~/assets/css/global.css', '~/node_modules/bootstrap/dist/css/bootstrap.min.css'],
     fonts: ['~/assets/fonts/noe-display-bold.ttf'],
     plugins: ['~/plugins/bootstrap.client'],
     server: {
-        //port : 3002,
+        port: 3002,
+        host: '0.0.0.0', // Écoute sur toutes les interfaces
         https: {
-          key: fs.readFileSync(path.resolve(__dirname, 'server.key')),
-          cert: fs.readFileSync(path.resolve(__dirname, 'server.crt'))
+            // key: fs.readFileSync(path.resolve(__dirname, 'server.key')),
+            // cert: fs.readFileSync(path.resolve(__dirname, 'server.crt'))
         },
     },
     vite: {
-      server: {
-        https: true,
-        host: true,
-        hmr: {
-          protocol: 'wss',
-          host: 'wajitech.fr',
-          clientPort: 3002
+        server: {
+            https: true,
+            host: true, // Écoute sur toutes les interfaces
+            hmr: {
+                protocol: 'wss',
+                host: '0.0.0.0', // Ou 'localhost' si nécessaire
+                clientPort: 9444 // Port exposé sur la machine hôte
+            }
         }
-      }
     }
 }
